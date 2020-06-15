a025f3c47a28833655868355ae1098c22c76e123
Nevill Wilder <wildercontact@icloud.com>
2020-06-15 17:28:03 -0600

    Mock File added
    Currently includes a declaration of __unimplemented. Will ultimately be used to make a mock version for tests

---
+++ Sources/ComposablePhotoKit/Mock.swift
@@ -0,0 +1,22 @@
+//
+//  Mock.swift
+//  RecollMobile
+//
+//  Created by Nevill Wilder on 6/15/20.
+//  Copyright © 2020 NevWild. All rights reserved.
+//
+
+import Foundation
+
+public func _unimplemented(
+  _ function: StaticString, file: StaticString = #file, line: UInt = #line
+) -> Never {
+  fatalError(
+    """
+    `\(function)` was called but is not implemented. Be sure to provide an implementation for
+    this endpoint when creating the mock.
+    """,
+    file: file,
+    line: line
+  )
+}
