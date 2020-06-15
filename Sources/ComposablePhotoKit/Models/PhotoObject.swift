8a73560fc9cb22ec9188ec507058ce238876d8cf
Nevill Wilder <wildercontact@icloud.com>
2020-06-15 17:29:15 -0600

    PhotoObject Added

---
+++ Sources/ComposablePhotoKit/Models/PhotoObject.swift
@@ -0,0 +1,31 @@
+//
+//  PhotoObject.swift
+//  RecollMobile
+//
+//  Created by Nevill Wilder on 6/15/20.
+//  Copyright © 2020 NevWild. All rights reserved.
+//
+
+import Photos
+
+
+public struct PhotoObject: Equatable {
+
+    public let rawValue: PHObject?
+
+    public var localIdentifier: String
+
+    public init(PhotoObject: PHObject) {
+        self.rawValue = nil
+        self.localIdentifier = PhotoObject.localIdentifier
+    }
+
+    public init(localIdentifier: String){
+        self.rawValue = nil
+        self.localIdentifier = localIdentifier
+    }
+
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+        lhs.localIdentifier == rhs.localIdentifier
+    }
+}
