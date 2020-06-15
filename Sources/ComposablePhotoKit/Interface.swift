d02649d16d095d865768dac8104775fe05321ade
Nevill Wilder <wildercontact@icloud.com>
2020-06-15 17:26:50 -0600

    Add Interface file for PHPhotoLibrary
    This interface defines how TCA interacts with the PhotoLibrary

---
+++ Sources/ComposablePhotoKit/Interface.swift
@@ -0,0 +1,139 @@
+//
+//  Interface.swift
+//  RecollMobile
+//
+//  Created by Nevill Wilder on 6/12/20.
+//  Copyright © 2020 NevWild. All rights reserved.
+//
+
+import Combine
+import ComposableArchitecture
+import Photos
+import PhotosUI
+
+//TODO: Figure out where to put the perform changes methods of the photolibrary. I intuiit that they should be actions, but I'm not sure how to handle objc blocks in the tca.
+
+public struct PhotoLibrary {
+
+    var sharedPhotoLibrary:PHPhotoLibrary =  {_ in PHPhotoLibrary.shared()}
+
+    public enum Action : Equatable {
+        case photoLibraryDidChange(PHChange)
+        case photoLibraryDidBecomeUnavailable
+    }
+    public struct Error: Swift.Error, Equatable {
+        public let error: NSError
+
+        public init(_ error: Swift.Error) {
+            self.error = error as NSError
+        }
+    }
+
+    public var authorizationStatus: () -> PHAuthorizationStatus = {
+        _unimplemented("authorizationStatus")
+    }
+
+    var requestAuthorization: (AnyHashable) -> Effect<Never, Never> = { _ in
+        _unimplemented("requestAuthorization")
+    }
+
+    // TODO: confirm this is the right way to implement this
+    var unavailabilityReason: Error = {_ in _unimplemented("requestAuthorization")}
+
+    var registerAvailabilityObserver: (AnyHashable) -> Effect<Never,Never> = {_ in _unimplemented("registerAvailabilityObserver")}
+
+    var unregisterAvailabilityObserver: (AnyHashable) -> Effect<Never,Never> = {_ in _unimplemented("unregisterAvailabilityObserver")}
+
+    var registerChangeObserver: (AnyHashable) -> Effect<Never, Never> = {_ in _unimplemented("registerChangeObserver")}
+
+    var unregisterChangeObserver: (AnyHashable) -> Effect<Never, Never> = {_ in _unimplemented("unregisterChangeObserver")}
+
+    public func requestAuthorization(id: AnyHashable) -> Effect<Never, Never> {
+        self.requestAuthorization(id)
+    }
+
+
+}
+
+
+
+// MARK: PHPhotoLibrary OBJC Header
+
+/*
+ //
+ //  PHPhotoLibrary.h
+ //  Photos
+ //
+ //  Copyright (c) 2013 Apple Inc. All rights reserved.
+ //
+
+ #import <Foundation/Foundation.h>
+ #import <Photos/PhotosTypes.h>
+
+ NS_ASSUME_NONNULL_BEGIN
+ API_AVAILABLE_BEGIN(macos(10.13), ios(8), tvos(10))
+
+ @class PHChange;
+ @class PHPhotoLibrary;
+
+ typedef NS_ENUM(NSInteger, PHAuthorizationStatus) {
+ PHAuthorizationStatusNotDetermined = 0, // User has not yet made a choice with regards to this application
+ PHAuthorizationStatusRestricted,        // This application is not authorized to access photo data.
+ // The user cannot change this application’s status, possibly due to active restrictions
+ //   such as parental controls being in place.
+ PHAuthorizationStatusDenied,            // User has explicitly denied this application access to photos data.
+ PHAuthorizationStatusAuthorized         // User has authorized this application to access photos data.
+ };
+
+ #pragma mark -
+ @protocol PHPhotoLibraryChangeObserver <NSObject>
+ // This callback is invoked on an arbitrary serial queue. If you need this to be handled on a specific queue, you should redispatch appropriately
+ - (void)photoLibraryDidChange:(PHChange *)changeInstance;
+
+ @end
+
+ #pragma mark -
+ API_AVAILABLE(macos(10.15), ios(13), tvos(13))
+ @protocol PHPhotoLibraryAvailabilityObserver <NSObject>
+ // This notification is posted on a private queue.
+ - (void)photoLibraryDidBecomeUnavailable:(PHPhotoLibrary *)photoLibrary API_AVAILABLE(macos(10.15), ios(13), tvos(13));
+ @end
+
+ /*!
+ @class        PHPhotoLibrary
+ @abstract     A PHPhotoLibrary provides access to the metadata and image data for the photos, videos and related content in the user's photo library, including content from the Camera Roll, iCloud Shared, Photo Stream, imported, and synced from iTunes.
+ @discussion   ...
+ */
+ #pragma mark -
+ OS_EXPORT
+ @interface PHPhotoLibrary : NSObject
+
+ + (PHPhotoLibrary *)sharedPhotoLibrary;
+
+ + (PHAuthorizationStatus)authorizationStatus;
+ + (void)requestAuthorization:(void(^)(PHAuthorizationStatus status))handler;
+
+ #pragma mark - Library availability
+
+ @property (readonly, atomic, nullable, strong) NSError *unavailabilityReason API_AVAILABLE(macos(10.15), ios(13), tvos(13));
+
+ - (void)registerAvailabilityObserver:(id<PHPhotoLibraryAvailabilityObserver>)observer API_AVAILABLE(macos(10.15), ios(13), tvos(13));
+ - (void)unregisterAvailabilityObserver:(id<PHPhotoLibraryAvailabilityObserver>)observer API_AVAILABLE(macos(10.15), ios(13), tvos(13));
+
+ #pragma mark - Applying Changes
+
+ // handlers are invoked on an arbitrary serial queue
+ // Nesting change requests will throw an exception
+ - (void)performChanges:(dispatch_block_t)changeBlock completionHandler:(nullable void(^)(BOOL success, NSError *__nullable error))completionHandler;
+ - (BOOL)performChangesAndWait:(dispatch_block_t)changeBlock error:(NSError *__autoreleasing *)error;
+
+ #pragma mark - Change Handling
+
+ - (void)registerChangeObserver:(id<PHPhotoLibraryChangeObserver>)observer;
+ - (void)unregisterChangeObserver:(id<PHPhotoLibraryChangeObserver>)observer;
+
+ @end
+
+ API_AVAILABLE_END
+ NS_ASSUME_NONNULL_END
+ */
