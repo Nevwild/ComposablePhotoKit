1377eb44b3b89fdb3e7bd27c54f72e75a0ee6a31
Nevill Wilder <wildercontact@icloud.com>
2020-06-16 09:28:03 -0600

    PhotoObjectPlaceHolder Added

---
+++ Sources/ComposablePhotoKit/Models/PhotoObjectPlaceholder.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoObjectPlaceholder.swift
+//  
+//  Created by Nevill Wilder on 6/16/20.
+//  Copyright © 2020 NevWild. All rights reserved.
+//
+
+import Foundation
+
+
+// TODO: How do I represent PHObjectPlaceholder?
+// .h file representation:
+// @interface PHObjectPlaceholder : PHObject
+// @end
+// Hypothesized implementation
+// public let photoObjectPlaceholder: PHObject?
+// Do I need to represent this type? Where would I represent it?
+//Could this just be a typealias?
