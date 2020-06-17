31cfed2e849fff8cbb365e0c0d9b0f2f978d26b9
Nevill Wilder <wildercontact@icloud.com>
2020-06-17 11:05:09 -0600

    Packages set up

--- Tests/ComposablePhotoKitTests/ComposablePhotoKitTests.swift
+++ Tests/ComposablePhotoKitTests/ComposablePhotoKitTests.swift
@@ -6,7 +6,7 @@ final class ComposablePhotoKitTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(ComposablePhotoKit().text, "Hello, World!")
+       
     }
 
     static var allTests = [
